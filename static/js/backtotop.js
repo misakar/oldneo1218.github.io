f5749a08511ecde1355a797894050d7297aa0848
neo1218 <neo1218@yeah.net>
2016-12-11 12:10:27 +0800

    ship site update...✅

--- static/js/backtotop.js
+++ static/js/backtotop.js
@@ -1,5 +1,6 @@
 $(document).ready(function ($) {
     $.scrollto = function (scrolldom, scrolltime) {
+        // 点击死猫, 调用function函数
         $(scrolldom).click(function () {
             var scrolltodom = $(this).attr("data-scroll");
             $(this).addClass("active").siblings().removeClass("active");
@@ -10,9 +11,10 @@ $(document).ready(function ($) {
         });
     };
     
-    if ($(window).width() > 800) {
+    if ($(window).width()) {
         var backTo = $("#back-to-top");
-        var backHeight = $(window).height() - 900 + 'px';
+        // var backHeight = $(window).height() - 900 + 'px';
+        var backHeight = '-100px';
         $(window).scroll(function () {
             if ($(window).scrollTop() > 700 && backTo.css('top') === '-900px') {
                 backTo.css('top', backHeight);
